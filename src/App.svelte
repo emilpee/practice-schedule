8bfc790e8bba8cdcd015a705ea871026ec42b279
emilpee <emil.petersson@dentech.se>
2023-06-16 16:30:24 +0200

    Project init

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import Calendar from './lib/calendar/Calendar.svelte';
+</script>
+
+<main> 
+  <h1>Practice Schedule</h1>
+  <div class="card">
+    <Calendar />
+  </div>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/calendar/Calendar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+  let calendarEl: HTMLElement = document.getElementById('calendar')!;
+
+  let calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin ]
+    // options here
+  });
+
+  calendar.render();
+});
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here --> 
+
+<div id="calendar"></div>
