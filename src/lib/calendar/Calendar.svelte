d88b98cd65b483cdf9f17c589d8a026c1b8a0a5f
emilpee <emil.petersson@dentech.se>
2023-06-30 16:24:08 +0200

    Get data from firestore

--- src/lib/calendar/Calendar.svelte
+++ src/lib/calendar/Calendar.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
-    import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
+  import { Calendar } from "@fullcalendar/core";
+  import dayGridPlugin from "@fullcalendar/daygrid";
 
-document.addEventListener('DOMContentLoaded', function() {
-  let calendarEl: HTMLElement = document.getElementById('calendar')!;
+  import { db } from "../../config/firestore";
+  import { collection, getDocs } from "firebase/firestore";
 
-  let calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
-    // options here
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "appointments"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
+  };
+
+  getData();
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let calendarEl: HTMLElement = document.getElementById("calendar")!;
+
+    let calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin],
+      // options here
+    });
+
+    calendar.render();
   });
-
-  calendar.render();
-});
 </script>
 
+<div id="calendar" />
+
 <style>
-    /* your styles go here */
 </style>
-
-<!-- markup (zero or more items) goes here --> 
-
-<div id="calendar"></div>
