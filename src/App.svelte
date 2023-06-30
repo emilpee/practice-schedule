2238c98668265248a8edc04459c1895f924332c5
emilpee <emil.petersson@dentech.se>
2023-06-30 16:23:35 +0200

    Clear imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import Calendar from './lib/calendar/Calendar.svelte';
+  import Calendar from "./lib/calendar/Calendar.svelte";
 </script>
 
-<main> 
+<main>
   <h1>Practice Schedule</h1>
-  <div class="card">
+  <div>
     <Calendar />
   </div>
 </main>
 
 <style>
-
 </style>
